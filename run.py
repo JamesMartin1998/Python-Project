689498edc620288a34385b78ccd6a3b693316342
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2022-06-20 10:31:41 +0000

    add words lists

--- run.py
+++ run.py
@@ -1,10 +1,17 @@
 print("Welcome to Hangman!\nTry to guess the word by guessing individual letters inside!")
 
+easy_words = ["cat", "sun", "cup", "ghost", "flower", "pie", "cow", "banana", "snowflake", "bug" "book", "jar", "snake", "light", "tree"]
+medium_words = ["backbone", "whistle", "palace", "baseball", "computer", "password", "spring", "toast", "outside", "photograph", "circus", "battery", "bicycle", "music", "pirate"]
+hard_words = ["avenue", "buffalo", "dwarves", "espionage", "galvanise", "injury", "jukebox", "kiosk", "luxury", "matrix", "oxygen", "queue", "rhythm", "transcript", "xylophone"]
+
 # Request diffculty function
 
 # Validate difficulty function
 
 # Generate word function
+
+# def generate_word():
+
 
 # Request letter function
 
