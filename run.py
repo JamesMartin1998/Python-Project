f757ae87b2b3ac0e045a275009e20a49739ef98f
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2022-06-20 10:00:07 +0000

    add peseudocode to represent layout and functions needed in the project

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print("Welcome to Hangman!\nTry to guess the word by guessing individual letters inside!")
+
+# Request diffculty function
+
+# Validate difficulty function
+
+# Generate word function
+
+# Request letter function
+
+# Validate letter function
+
+# Check letter in word function
+
+# Decrement lives function
+
+# Show current guess function
